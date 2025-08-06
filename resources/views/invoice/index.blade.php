362b63b6d9741b7d900f53f59c4e769a9106c78b
abiruzzamanabir <abiruzzamanabir17@gmail.com>
2025-08-06 12:50:50 +0600

    fix bug

--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -84,7 +84,7 @@
             </div>
         </div>
     </div>
-    @include('kill')
+    @include('feature.kill')
     <script src="https://code.jquery.com/jquery-3.6.3.slim.min.js"
         integrity="sha256-ZwqZIVdD3iXNyGHbSYdsmWP//UBokj2FHAxKuSBKDSo=" crossorigin="anonymous"></script>
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
